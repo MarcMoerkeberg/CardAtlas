3e90f02173f30911594e5cd0a0358f3efc110aa5
Marc Moerkeberg <mc.christiansen@outlook.com>
2025-01-14 22:17:37 +0100

    Add nullcheck for appsettings.json

--- CardAtlas.Server/Extensions/ApplicationBuilderExtensions.cs
+++ CardAtlas.Server/Extensions/ApplicationBuilderExtensions.cs
@@ -1,31 +1,31 @@
 ﻿using CardAtlas.Server.DAL;
+using CardAtlas.Server.Guards;
 using CardAtlas.Server.Models.Internal;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
-namespace CardAtlas.Server.Extensions
-{
-	public static class ApplicationBuilderExtensions
-	{
-		/// <summary>
-		/// Adds the database context and connection for <see cref="ApplicationDbContext"/>.
-		/// </summary>
-		/// <param name="services"></param>
-		public static void AddDatabaseContext(this IServiceCollection services)
-		{
-			AppSettings appSettings = services.BuildServiceProvider().GetRequiredService<IOptions<AppSettings>>().Value;
+namespace CardAtlas.Server.Extensions;
 
-			services.AddDbContext<ApplicationDbContext>(options =>
+public static class ApplicationBuilderExtensions
+{
+	/// <summary>
+	/// Adds the database context and connection for <see cref="ApplicationDbContext"/>.
+	/// </summary>
+	public static void AddDatabaseContext(this IServiceCollection services)
+	{
+		AppSettings? appSettings = services.BuildServiceProvider().GetRequiredService<IOptions<AppSettings>>().Value;
+		if (!appSettings.HasValidConnectionStrings()) throw new NullReferenceException("Could not initialize AppSettings from appsettings.json.");
+
+		services.AddDbContext<ApplicationDbContext>(options =>
+		{
+			options.UseSqlServer(appSettings.ConnectionStrings.Database, sqlServerOptions =>
 			{
-				options.UseSqlServer(appSettings.ConnectionStrings.Database, sqlServerOptions =>
-				{
-					sqlServerOptions.EnableRetryOnFailure(
-						maxRetryCount: 5,
-						maxRetryDelay: TimeSpan.FromSeconds(20),
-						errorNumbersToAdd: null
-					);
-				});
+				sqlServerOptions.EnableRetryOnFailure(
+					maxRetryCount: 5,
+					maxRetryDelay: TimeSpan.FromSeconds(20),
+					errorNumbersToAdd: null
+				);
 			});
-		}
+		});
 	}
 }
---
+++ CardAtlas.Server/Guards/AppSettingGuards.cs
@@ -0,0 +1,13 @@
+﻿using CardAtlas.Server.Models.Internal;
+
+namespace CardAtlas.Server.Guards;
+
+public static class AppSettingGuards
+{
+	public static bool HasValidConnectionStrings(this AppSettings? appSettings)
+	{
+		return appSettings != null
+			&& appSettings.ConnectionStrings != null
+			&& !string.IsNullOrWhiteSpace(appSettings.ConnectionStrings.Database);
+	}
+}
