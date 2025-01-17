6d1d515734e917a23ff89169d855e08c12bba396
Marc Christiansen <mch@e-komplet.dk>
2025-01-17 13:33:04 +0100

    Make method for getting appsettings model from appsettings.json

--- CardAtlas.Server/Extensions/ApplicationBuilderExtensions.cs
+++ CardAtlas.Server/Extensions/ApplicationBuilderExtensions.cs
@@ -9,12 +9,25 @@ namespace CardAtlas.Server.Extensions;
 public static class ApplicationBuilderExtensions
 {
 	/// <summary>
+	/// Returns the <see cref="AppSettings"/> object from the <see cref="IServiceCollection"/>.<br/>
+	/// Throws a <see cref="NullReferenceException"/> if the <see cref="AppSettings"/> object is null. This is usually due to an invalid appsettings.json file or the buildprovider not being properly configured.
+	/// </summary>
+	/// <exception cref="NullReferenceException"></exception>
+	private static AppSettings GetAppSettings(this IServiceCollection services)
+	{
+		AppSettings? appSettings = services.BuildServiceProvider().GetRequiredService<IOptions<AppSettings>>().Value;
+		if (appSettings == null) throw new NullReferenceException("Could not initialize AppSettings from appsettings.json.");
+
+		return appSettings;
+	}
+
+	/// <summary>
 	/// Adds the database context and connection for <see cref="ApplicationDbContext"/>.
 	/// </summary>
 	public static void AddDatabaseContext(this IServiceCollection services)
 	{
-		AppSettings? appSettings = services.BuildServiceProvider().GetRequiredService<IOptions<AppSettings>>().Value;
-		if (!appSettings.HasValidConnectionStrings()) throw new NullReferenceException("Could not initialize AppSettings from appsettings.json.");
+		AppSettings appSettings = GetAppSettings(services);
+		if(appSettings.HasValidConnectionStrings()) throw new NullReferenceException("Could not initialize connectionstrings from appsettings.json when adding the dbcontext.");
 
 		services.AddDbContext<ApplicationDbContext>(options =>
 		{
--- CardAtlas.Server/Guards/AppSettingGuards.cs
+++ CardAtlas.Server/Guards/AppSettingGuards.cs
@@ -4,10 +4,5 @@ namespace CardAtlas.Server.Guards;
 
 public static class AppSettingGuards
 {
-	public static bool HasValidConnectionStrings(this AppSettings? appSettings)
-	{
-		return appSettings != null
-			&& appSettings.ConnectionStrings != null
-			&& !string.IsNullOrWhiteSpace(appSettings.ConnectionStrings.Database);
-	}
+	public static bool HasValidConnectionStrings(this AppSettings appSettings) => appSettings.ConnectionStrings != null && !string.IsNullOrWhiteSpace(appSettings.ConnectionStrings.Database);
 }
