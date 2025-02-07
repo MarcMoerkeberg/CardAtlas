97202de4829c25b9f7e10358acdd8d45556a1778
Marc Christiansen <mch@e-komplet.dk>
2025-02-07 15:24:03 +0100

    Jank enum parsing

--- CardAtlas.Server/Helpers/ScryfallHelper.cs
+++ CardAtlas.Server/Helpers/ScryfallHelper.cs
@@ -9,10 +9,15 @@ public static class ScryfallHelper
 
 	public static TEnum ParseAsScryfallEnum<TEnum>(this string target) where TEnum : struct, Enum
 	{
-		string normalizedTarget = target.Replace("_", string.Empty);
+		string normalizedTarget = NormalizeStringForEnumParsing(target);
 		return Enum.TryParse(normalizedTarget, ignoreCase: true, out TEnum parsedResult)
 			? parsedResult
 			: default;
+	}
+
+	private static string NormalizeStringForEnumParsing(string target)
+	{
+		return target.Replace("_", string.Empty);
 	}
 
 	public static Dictionary<TKey, TValue> ToEnumKeyDictionary<TKey, TValue>(this Dictionary<string, string>? targetDictionary) where TKey : struct, Enum
@@ -47,68 +52,41 @@ public static class ScryfallHelper
 
 		return type switch
 		{
-			_ when type == typeof(bool) => bool.TryParse(target, out bool boolValue) 
-				? (true, (T)(object)boolValue) 
-				: errorResult,
-
-			_ when type == typeof(byte) => byte.TryParse(target, out byte byteValue) 
-				? (true, (T)(object)byteValue) 
-				: errorResult,
-
-			_ when type == typeof(char) => char.TryParse(target, out char charValue) 
-				? (true, (T)(object)charValue) 
-				: errorResult,
-			
-			_ when type == typeof(decimal) => decimal.TryParse(target, out decimal decimalValue) 
-				? (true, (T)(object)decimalValue) 
-				: errorResult,
-
-			_ when type == typeof(double) => double.TryParse(target, out double doubleValue) 
-				? (true, (T)(object)doubleValue) 
-				: errorResult,
-
-			_ when type == typeof(float) => float.TryParse(target, out float floatValue) 
-				? (true, (T)(object)floatValue) 
-				: errorResult,
-
+			_ when type == typeof(bool) => (bool.TryParse(target, out bool boolValue), (T)(object)boolValue),
+			_ when type == typeof(byte) => (byte.TryParse(target, out byte byteValue), (T)(object)byteValue),
+			_ when type == typeof(char) => (char.TryParse(target, out char charValue), (T)(object)charValue),
+			_ when type == typeof(decimal) => (decimal.TryParse(target, out decimal decimalValue), (T)(object)decimalValue),
+			_ when type == typeof(double) => (double.TryParse(target, out double doubleValue), (T)(object)doubleValue),
+			_ when type == typeof(float) => (float.TryParse(target, out float floatValue), (T)(object)floatValue),
 			_ when type == typeof(int) => (int.TryParse(target, out int intValue), (T)(object)intValue),
-				//? (true, (T)(object)intValue) 
-				//: errorResult,
-
-			_ when type == typeof(long) => long.TryParse(target, out long longValue) 
-				? (true, (T)(object)longValue) 
-				: errorResult,
-
-			_ when type == typeof(sbyte) => sbyte.TryParse(target, out sbyte sbyteValue) 
-				? (true, (T)(object)sbyteValue) 
-				: errorResult,
-
-			_ when type == typeof(short) => short.TryParse(target, out short shortValue) 
-				? (true, (T)(object)shortValue) 
-				: errorResult,
-
-			_ when type == typeof(uint) => uint.TryParse(target, out uint uintValue) 
-				? (true, (T)(object)uintValue) 
-				: errorResult,
-
-			_ when type == typeof(ulong) => ulong.TryParse(target, out ulong ulongValue) 
-				? (true, (T)(object)ulongValue) 
-				: errorResult,
-
-			_ when type == typeof(ushort) => ushort.TryParse(target, out ushort ushortValue) 
-				? (true, (T)(object)ushortValue) 
-				: errorResult,
-
-			_ when type.IsEnum => (true, ParseEnum<T>(target)),
+			_ when type == typeof(long) => (long.TryParse(target, out long longValue),(T)(object)longValue),
+			_ when type == typeof(sbyte) => (sbyte.TryParse(target, out sbyte sbyteValue), (T)(object)sbyteValue),
+			_ when type == typeof(short) => (short.TryParse(target, out short shortValue), (T)(object)shortValue),
+			_ when type == typeof(uint) => (uint.TryParse(target, out uint uintValue), (T)(object)uintValue),
+			_ when type == typeof(ulong) => (ulong.TryParse(target, out ulong ulongValue), (T)(object)ulongValue),
+			_ when type == typeof(ushort) => (ushort.TryParse(target, out ushort ushortValue), (T)(object)ushortValue),
+			_ when type.IsEnum => ParseEnum<T>(target),
 			_ => (false, default!)
 		};
 	}
 
-	private static T ParseEnum<T>(string value)
+	private static (bool parsingResult, T parsedValue) ParseEnum<T>(string value)
 	{
-		Type enumType = typeof(T);
-		var test = Nullable.GetUnderlyingType(enumType);
+		var result = (false, default(T)!);
 
-		return value.ParseAsScryfallEnum<>();
+		try
+		{
+			Type enumType = typeof(T);
+			string normalizedTarget = NormalizeStringForEnumParsing(value);
+
+			T parsedEnum = (T)Enum.Parse(enumType, normalizedTarget, ignoreCase: true);
+
+			result = (true, parsedEnum);
+		}
+		catch (Exception)
+		{
+		}
+		
+		return result;
 	}
 }
