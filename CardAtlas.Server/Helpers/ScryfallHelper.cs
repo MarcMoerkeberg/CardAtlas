340cd73f2cc139c057864853e4f573a28dc1b880
Marc Moerkeberg <mc.christiansen@outlook.com>
2025-02-09 22:17:04 +0100

    Helper methods for binding properties from strings to models

--- CardAtlas.Server/Helpers/ScryfallHelper.cs
+++ CardAtlas.Server/Helpers/ScryfallHelper.cs
@@ -1,4 +1,7 @@
-﻿namespace CardAtlas.Server.Helpers;
+﻿using System.Collections.Concurrent;
+using System.Reflection;
+
+namespace CardAtlas.Server.Helpers;
 
 public static class ScryfallHelper
 {
@@ -173,5 +176,79 @@ public static class ScryfallHelper
 		if (!Uri.IsWellFormedUriString(value, UriKind.Absolute)) return errorResult;
 
 		return (Uri.TryCreate(value, UriKind.Absolute, out Uri? uriResult), (T)(object)uriResult!);
+	}
+
+	/// <summary>
+	/// Caches the properties for each type, using a case-insensitive dictionary keyed by property name.
+	/// </summary>
+	private static readonly ConcurrentDictionary<Type, Dictionary<string, PropertyInfo>> _typeProperties =
+		new ConcurrentDictionary<Type, Dictionary<string, PropertyInfo>>();
+
+	/// <summary>
+	/// Gets a dictionary of public instance properties for <paramref name="modelType"/>.
+	/// </summary>
+	/// <param name="modelType">The type of the model to retrieve properties for.</param>
+	/// <returns>A dictionary with <see cref="PropertyInfo"/> as values and property names as keys for <paramref name="modelType"/>.</returns>
+	private static Dictionary<string, PropertyInfo> GetModelProperties(Type modelType)
+	{
+		Dictionary<string, PropertyInfo> properties = _typeProperties.GetOrAdd(modelType, type =>
+			type.GetProperties(BindingFlags.Instance | BindingFlags.Public)
+				.ToDictionary(property => property.Name, StringComparer.OrdinalIgnoreCase));
+
+		return properties;
+	}
+
+	/// <summary>
+	/// Returns a new <typeparamref name="TModel"/> with properties populated by <paramref name="parserFunction"/> if properties on the model matches the json string.<br/>
+	/// </summary>
+	/// <typeparam name="TModel">The type of the model to create and bind values to.</typeparam>
+	/// <param name="jsonStrings">A collection of JSON string keys corresponding to property names.</param>
+	/// <param name="parserFunction">If a matching property from <typeparamref name="TModel"/> is found, it uses this function to populate the propety.</param>
+	/// <returns>An instance of <typeparamref name="TModel"/> with properties populated by <paramref name="parserFunction"/> where <paramref name="jsonStrings"/> matches property names.</returns>
+	public static TModel BindJsonStringsToModel<TModel>(IEnumerable<string> jsonStrings, Func<string, object> parserFunction) where TModel : class, new()
+	{
+		var model = new TModel();
+		Dictionary<string, PropertyInfo> properties = GetModelProperties(typeof(TModel));
+
+		foreach (string jsonString in jsonStrings)
+		{
+			if (string.IsNullOrEmpty(jsonString))
+				continue;
+
+			if (properties.TryGetValue(jsonString, out PropertyInfo? property) && property is not null && property.CanWrite)
+			{
+				object propValue = parserFunction(jsonString);
+				property.SetValue(model, propValue);
+			}
+		}
+
+		return model;
+	}
+
+	/// <summary>
+	/// Creates a new instance of <typeparamref name="TModel"/> with populated properties from the keys and values from <paramref name="dictionary"/> and the <paramref name="parserFunction"/>.
+	/// </summary>
+	/// <typeparam name="TModel">The type of the model to create and bind values to.</typeparam>
+	/// <typeparam name="TTarget">The <see cref="Type"/> you want to parse values in <paramref name="dictionary"/> to.</typeparam>
+	/// <param name="dictionary">A dictionary with keys corresponding to property names and values of <typeparamref name="TModel"/>.<br/>
+	/// Values will be parsed with <paramref name="parserFunction"/>.
+	/// </param>
+	/// <param name="parserFunction">A function that parses a dictionary value of type <typeparamref name="TTarget"/> into an object for assignment.</param>
+	/// <returns>An instance of <typeparamref name="TModel"/> with properties populated by <paramref name="parserFunction"/> where <paramref name="dictionary"/> keys matches property names.</returns>
+	public static TModel BindDictionaryToModel<TModel, TTarget>(Dictionary<string, TTarget> dictionary, Func<TTarget, object?> parserFunction) where TModel : class, new()
+	{
+		var model = new TModel();
+		Dictionary<string, PropertyInfo> properties = GetModelProperties(typeof(TModel));
+
+		foreach (var keyValuePair in dictionary)
+		{
+			if (properties.TryGetValue(keyValuePair.Key, out PropertyInfo? property) && property is not null && property.CanWrite)
+			{
+				object? propValue = parserFunction(keyValuePair.Value);
+				property.SetValue(model, propValue);
+			}
+		}
+
+		return model;
 	}
 }
