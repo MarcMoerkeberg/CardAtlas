5cd39e811221af2616552a5928d89e9cf93e5cac
Marc Christiansen <mch@e-komplet.dk>
2025-02-14 14:12:33 +0100

    Add unit testing projekt with test for configuration helper.

---
+++ CardAtlas.UnitTests/EntityConfigurationTests.cs
@@ -0,0 +1,19 @@
+using CardAtlas.Server.Helpers;
+using CardAtlas.Server.Models.Data;
+
+namespace CardAtlas.UnitTests
+{
+	public class Tests
+	{
+		[Test]
+		public void GetEnumSeedData_CanCreateSeedData_ExpectsEntityTypePropertiesToBePopulated()
+		{
+			IEnumerable<Rarity> seedData = EntityConfigurationHelper.GetEnumSeedData<Rarity, RarityType>();
+
+			Assert.That(seedData, Is.Not.Null);
+			Assert.That(seedData, Is.Not.Empty);
+			Assert.That(seedData, Has.All.Property(nameof(Rarity.Id)).Not.Null);
+			Assert.That(seedData, Has.All.Property(nameof(Rarity.Name)).Not.Null);
+		}
+	}
+}
