27e66a8d6c4c1a8121afbc73be4d39d570119d88
Marc Christiansen <mch@e-komplet.dk>
2025-02-17 15:57:06 +0100

    Add types for Set object

--- CardAtlas.Server/Models/Data/Set.cs
+++ CardAtlas.Server/Models/Data/Set.cs
@@ -8,4 +8,43 @@ public class Set
 	[Key]
 	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 	public int Id { get; set; }
+	public Guid ScryfallId { get; set; }
+
+	[MinLength(1)]
+	[MaxLength(100)]
+	public required string Name { get; set; }
+	
+	[MinLength(3)]
+	[MaxLength(6)]
+	public required string Code { get; set; }	
+	
+	[MinLength(3)]
+	[MaxLength(6)]
+	public string? MtgoCode { get; set; }
+	
+	[MinLength(3)]
+	[MaxLength(6)]
+	public string? ArenaCode { get; set; }
+	
+	[MinLength(3)]
+	[MaxLength(6)]
+	public string? ParentSetCode { get; set; }
+
+	[MinLength(3)]
+	[MaxLength(6)]
+	public string? Block { get; set; }
+
+	[MinLength(3)]
+	[MaxLength(6)]
+	public string? BlockCode { get; set; }
+
+	[ForeignKey("SetTypeId")]
+	public required SetType SetType { get; set; }
+	public int SetTypeId { get; set; }
+	
+	public int NumberOfCards { get; set; }
+	public bool IsDigitalOnly { get; set; }
+	public bool IsFoilOnly { get; set; }
+	public bool IsNonFoilOnly { get; set; }
+	public DateOnly? ReleaseDate { get; set; }
 }
---
+++ CardAtlas.Server/Models/Data/SetType.cs
@@ -0,0 +1,38 @@
+﻿using CardAtlas.Server.Models.Data.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CardAtlas.Server.Models.Data;
+
+public class SetType : TypeEntity<SetKind>
+{
+	[InverseProperty("SetType")]
+	public required ICollection<Set> Sets { get; set; }
+}
+
+public enum SetKind
+{
+	Core = 1,
+	Expansion = 2,
+	Masters = 3,
+	Alchemy = 4,
+	Masterpiece = 5,
+	Arsenal = 6,
+	FromTheVault = 7,
+	Spellbook = 8,
+	PremiumDeck = 9,
+	DuelDeck = 10,
+	DraftInnovation = 11,
+	TreasureChest = 12,
+	Commander = 13,
+	Planechase = 14,
+	Archenemy = 15,
+	Vanguard = 16,
+	Funny = 17,
+	Starter = 18,
+	Box = 19,
+	Promo = 20,
+	Token = 21,
+	Memorabilia = 22,
+	MiniGame = 23,
+	NotImplemented = 0
+}
