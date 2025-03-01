305ee6ebf9cb60283fcf3ba17d1ee6eee41c7c22
Marc Christiansen <mch@e-komplet.dk>
2025-03-01 20:37:40 +0100

    Add default configuration for artists

---
+++ CardAtlas.Server/DAL/EntityConfigurations/ArtistConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CardAtlas.Server.Helpers;
+using CardAtlas.Server.Models.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CardAtlas.Server.DAL.EntityConfigurations;
+
+public class ArtistConfiguration : IEntityTypeConfiguration<Artist>
+{
+	public void Configure(EntityTypeBuilder<Artist> builder)
+	{
+		IEnumerable<Currency> seedData = EntityConfigurationHelper.GetEnumSeedData<Currency, CurrencyType>();
+
+		builder.HasData(
+			new Artist
+			{
+				Id = -1,
+				ScryfallId = null,
+				Name = "Unknown - Default artist"
+			}
+		);
+	}
+}
