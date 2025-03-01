6c427d92a668e88971c03c9e7c4c79f2d26bb43d
Marc Christiansen <mch@e-komplet.dk>
2025-03-01 20:40:50 +0100

    Add artist service and temporary stubs

---
+++ CardAtlas.Server/Services/ArtistService.cs
@@ -0,0 +1,26 @@
+﻿using CardAtlas.Server.Models.Data;
+using CardAtlas.Server.Services.Interfaces;
+
+namespace CardAtlas.Server.Services;
+
+public class ArtistService : IArtistService
+{
+	public ArtistService()
+	{
+	}
+
+	public Task<Artist?> GetFromScryfallId(Guid scryfallId)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Artist> Create(Artist artist)
+	{
+		throw new NotImplementedException();
+	}
+	
+	public Task<Artist> Get(int artistId)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ CardAtlas.Server/Services/Interfaces/IArtistService.cs
@@ -0,0 +1,11 @@
+﻿using CardAtlas.Server.Models.Data;
+
+namespace CardAtlas.Server.Services.Interfaces
+{
+	public interface IArtistService
+	{
+		Task<Artist> Create(Artist artist);
+		Task<Artist> Get(int artistId);
+		Task<Artist?> GetFromScryfallId(Guid scryfallId);
+	}
+}
