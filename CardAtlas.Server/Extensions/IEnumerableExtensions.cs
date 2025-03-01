e1c19d21473b1985f451c9f8e7477957d81b261d
Marc Christiansen <mch@e-komplet.dk>
2025-03-01 20:38:00 +0100

    Add IEnumerable extensions

---
+++ CardAtlas.Server/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace CardAtlas.Server.Extensions;
+
+public static class IEnumerableExtensions
+{
+	public static bool IsNullOrEmpty<T>(this IEnumerable<T>? enumerable)
+	{
+		return enumerable is null || !enumerable.Any();
+	}
+}
