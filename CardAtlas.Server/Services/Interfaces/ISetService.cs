3ecdab1c8bdc10e8e4a88b067d1f54d363b15fcb
Marc Christiansen <mch@e-komplet.dk>
2025-03-06 07:59:13 +0100

    Add setservice

---
+++ CardAtlas.Server/Services/Interfaces/ISetService.cs
@@ -0,0 +1,10 @@
+﻿using CardAtlas.Server.Models.Data;
+
+namespace CardAtlas.Server.Services.Interfaces;
+
+public interface ISetService
+{
+	public Task<Set?> GetFromScryfallId(Guid scryfallId);
+	public Task<Set> Create(Set set);
+	public Task<Set> Get(int setId);
+}
---
+++ CardAtlas.Server/Services/SetService.cs
@@ -0,0 +1,27 @@
+﻿using CardAtlas.Server.Models.Data;
+using CardAtlas.Server.Services.Interfaces;
+
+namespace CardAtlas.Server.Services;
+
+public class SetService : ISetService
+{
+	public SetService()
+	{
+	}
+
+
+	public Task<Set?> GetFromScryfallId(Guid scryfallId)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Set> Create(Set set)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Set> Get(int setId)
+	{
+		throw new NotImplementedException();
+	}
+}
