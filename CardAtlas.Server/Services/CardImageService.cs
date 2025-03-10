c2ac73872bc503ad19c049d997943e1b00155813
Marc Christiansen <mch@e-komplet.dk>
2025-03-10 23:17:33 +0100

    Added CardImageService with interface and method stubs.

---
+++ CardAtlas.Server/Services/CardImageService.cs
@@ -0,0 +1,32 @@
+﻿using CardAtlas.Server.Models.Data.Image;
+using CardAtlas.Server.Services.Interfaces;
+
+namespace CardAtlas.Server.Services;
+
+public class CardImageService : ICardImageService
+{
+	public Task<CardImage> Create(CardImage cardImage)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<CardImage> Get(long cardImageId)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IEnumerable<CardImage>> GetFromCardId(long cardId)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<CardImage> Update(CardImage cardImageWithChanges)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<CardImage> UpdateIfChanged(CardImage cardImageWithChanges)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ CardAtlas.Server/Services/Interfaces/ICardImageService.cs
@@ -0,0 +1,40 @@
+﻿using CardAtlas.Server.Models.Data.Image;
+
+namespace CardAtlas.Server.Services.Interfaces;
+
+public interface ICardImageService
+{
+	/// <summary>
+	/// Returns the <see cref="CardImage"/> from the db with the specified <paramref name="cardImageId"/>.<br/>
+	/// Throws an <see cref="InvalidOperationException"/> if no, or more than one <see cref="CardImage"/> is found.
+	/// </summary>
+	/// <exception cref="InvalidOperationException">Is thrown if no or more than one entity with that id is found.</exception>
+	public Task<CardImage> Get(long cardImageId);
+
+	/// <summary>
+	/// Adds the provided <paramref name="cardImage"/> to the database.
+	/// </summary>
+	/// <returns>The added <see cref="CardImage"/> with identity.</returns>
+	public Task<CardImage> Create(CardImage cardImage);
+
+	/// <summary>
+	/// Updates existing entity with changes from <paramref name="cardImageWithChanges"/>.<br/>
+	/// </summary>
+	/// <returns>The updated <see cref="CardImage"/>.</returns>
+	/// <exception cref="InvalidOperationException">Is thrown if no or more than one entity with that id is found.</exception>
+	public Task<CardImage> Update(CardImage cardImageWithChanges);
+
+	/// <summary>
+	/// Updates existing entity with changes from <paramref name="cardImageWithChanges"/>.<br/>
+	/// Does not update the if there are no changes.
+	/// </summary>
+	/// <returns>The updated <see cref="CardImage"/>.</returns>
+	/// <exception cref="InvalidOperationException">Is thrown if no or more than one entity with that id is found.</exception>
+	public Task<CardImage> UpdateIfChanged(CardImage cardImageWithChanges);
+
+	/// <summary>
+	/// Returns the <see cref="CardImage"/> entries from the db with the specified <paramref name="cardId"/>.
+	/// </summary>
+	/// <returns>All <see cref="CardImage"/> entries which references <paramref name="cardId"/>. Enumerable is empty if no matches is found.</returns>
+	public Task<IEnumerable<CardImage>> GetFromCardId(long cardId);
+}
