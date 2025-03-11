5ec59d685840ee9339e17fd903855699a2bf0aec
Marc Christiansen <mch@e-komplet.dk>
2025-03-11 12:42:57 +0100

    Update CardExtension method name to be more explicit

--- CardAtlas.Server/Extensions/CardExtensions.cs
+++ CardAtlas.Server/Extensions/CardExtensions.cs
@@ -9,7 +9,7 @@ public static class CardExtensions
 	/// Finds the first <see cref="Card"/> in <paramref name="targetCollection"/> with properties that match the provided <paramref name="cardFace"/>.
 	/// </summary>
 	/// <returns>The first <see cref="Card"/> with properties that match <paramref name="cardFace"/>. Returns null if none is found.</returns>
-	public static Card? FindMatchingCard(this IEnumerable<Card> targetCollection, CardFace cardFace)
+	public static Card? FindMatchByName(this IEnumerable<Card> targetCollection, CardFace cardFace)
 	{
 		return targetCollection.FirstOrDefault(cardFromDb => 
 			string.Equals(cardFromDb.Name, cardFace.Name)
