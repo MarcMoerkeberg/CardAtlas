f73ba56285043ded5d6dc0b04a7858fc543a99f1
Marc Christiansen <mch@e-komplet.dk>
2025-03-18 15:02:11 +0100

    Remove unit tests for removed extension method

--- CardAtlas.UnitTests/ExtensionTests/CardExtensionsTests.cs
+++ CardAtlas.UnitTests/ExtensionTests/CardExtensionsTests.cs
@@ -2,78 +2,11 @@
 using CardAtlas.Server.Models.Data;
 using CardAtlas.Server.Models.Data.CardRelations;
 using CardAtlas.UnitTests.DataHelpers;
-using CardFace = ScryfallApi.Models.CardFace;
 
 namespace CardAtlas.UnitTests.ExtensionTests;
 
 class CardExtensionsTests
 {
-	[Test]
-	public void FindMatchingCard_ByCardFace_ExpectsNull_WhenCollectionIsEmpty()
-	{
-		IEnumerable<Card> targetCollection = new List<Card>();
-		CardFace cardFace = CardDataHelper.CreateCardFace();
-		
-		Card? result = targetCollection.FindMatchByName(cardFace);
-		
-		Assert.That(result, Is.Null);
-	}
-	
-	[Test]
-	public void FindMatchingCard_ByCardFace_ExpectsSingleCard_WhenCollectionIsPopulated()
-	{
-		string expectedName = "First card";
-
-		IEnumerable<Card> targetCollection = new List<Card>
-		{
-			CardDataHelper.CreateCard(name: expectedName),
-			CardDataHelper.CreateCard(name: "Second card"),
-		};
-
-		CardFace cardFace = CardDataHelper.CreateCardFace(name: expectedName);
-
-		Card? result = targetCollection.FindMatchByName(cardFace);
-		
-		Assert.That(result, Is.Not.Null);
-		Assert.That(result?.Name, Is.EqualTo(expectedName));
-	}
-	
-	[Test]
-	public void FindMatchingCard_ByCardFace_ExpectsFirstCard_WhenCollectionContainsMultipleMatches()
-	{
-		string expectedName = "First card";
-
-		IEnumerable<Card> targetCollection = new List<Card>
-		{
-			CardDataHelper.CreateCard(name: expectedName),
-			CardDataHelper.CreateCard(name: expectedName),
-		};
-
-		CardFace cardFace = CardDataHelper.CreateCardFace(name: expectedName);
-
-		Card? result = targetCollection.FindMatchByName(cardFace);
-		
-		Assert.That(result, Is.Not.Null);
-		Assert.That(result?.Name, Is.EqualTo(expectedName));
-		Assert.That(targetCollection.First(), Is.EqualTo(result));
-	}
-	
-	[Test]
-	public void FindMatchingCard_ByCardFace_ExpectsNull_WhenCollectionContainsNoMatches()
-	{
-		IEnumerable<Card> targetCollection = new List<Card>
-		{
-			CardDataHelper.CreateCard(),
-			CardDataHelper.CreateCard(),
-		};
-
-		CardFace cardFace = CardDataHelper.CreateCardFace(name: "Name with no matches");
-
-		Card? result = targetCollection.FindMatchByName(cardFace);
-
-		Assert.That(result, Is.Null);
-	}
-
 	[Test]
 	public void FindMatchByVendorAndCurrency_ExpectsNull_WhenCollectionIsEmpty()
 	{
