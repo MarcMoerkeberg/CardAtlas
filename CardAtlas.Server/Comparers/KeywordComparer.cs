9c50aa091a3249d93d38f52e6bacf3863e823812
Marc Moerkeberg <mc.christiansen@outlook.com>
2025-03-23 08:47:51 +0100

    Add a keyword comparerer

---
+++ CardAtlas.Server/Comparers/KeywordComparer.cs
@@ -0,0 +1,29 @@
+﻿using CardAtlas.Server.Models.Data;
+
+namespace CardAtlas.Server.Comparers;
+
+public class KeywordComparer : IEqualityComparer<Keyword>
+{
+	public bool Equals(Keyword? x, Keyword? y)
+	{
+		if (x is null && y is null) return true;
+		if (x is null || y is null) return false;
+
+		return x.Id == y.Id
+			&& x.SourceId == y.SourceId
+			&& string.Equals(x.Name, y.Name)
+			&& string.Equals(x.ReminderText, y.ReminderText)
+			&& string.Equals(x.ReminderText, y.ReminderText);
+	}
+
+	public int GetHashCode(Keyword obj)
+	{
+		var hash = new HashCode();
+		hash.Add(obj.Id);
+		hash.Add(obj.Name);
+		hash.Add(obj.ReminderText);
+		hash.Add(obj.ReminderText);
+
+		return hash.ToHashCode();
+	}
+}
