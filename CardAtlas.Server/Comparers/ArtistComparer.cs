9d20d708125228ff6edd139d853b1bd0302d3c3e
Marc Moerkeberg <mc.christiansen@outlook.com>
2025-05-24 21:08:34 +0200

    Add artist comparer

---
+++ CardAtlas.Server/Comparers/ArtistComparer.cs
@@ -0,0 +1,27 @@
+﻿using CardAtlas.Server.Models.Data;
+
+namespace CardAtlas.Server.Comparers;
+
+public class ArtistComparer : IEqualityComparer<Artist>
+{
+	public bool Equals(Artist? x, Artist? y)
+	{
+		if (x is null && y is null) return true;
+		if (x is null || y is null) return false;
+
+		return x.ScryfallId == y.ScryfallId
+			&& x.Id == y.Id
+			&& string.Equals(x.Name, y.Name);
+	}
+
+	public int GetHashCode(Artist obj)
+	{
+		var hash = new HashCode();
+		hash.Add(obj.ScryfallId);
+		hash.Add(obj.Id);
+		hash.Add(obj.Name);
+
+		return hash.ToHashCode();
+	}
+
+}
