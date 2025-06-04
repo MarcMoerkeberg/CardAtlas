2d03714563f3b385b39c497f456176e9171f4aa9
Marc Christiansen <mch@e-komplet.dk>
2025-06-04 17:42:55 +0200

    Add missing summary for extension method

--- CardAtlas.Server/Extensions/StringExtensions.cs
+++ CardAtlas.Server/Extensions/StringExtensions.cs
@@ -2,6 +2,11 @@
 
 public static class StringExtensions
 {
+	/// <summary>
+	/// Capitalizes the first letter of the specified string.
+	/// </summary>
+	/// <returns>A new string with the first letter capitalized. Returns an empty string if
+	/// <paramref name="target"/> is null, empty, or consists only of whitespace.</returns>
 	public static string CapitalizeFirstLetter(this string? target)
 	{
 		if (string.IsNullOrWhiteSpace(target)) return string.Empty;
