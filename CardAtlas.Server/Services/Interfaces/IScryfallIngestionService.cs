2e4e6b6a16730d9b55ad04bd4ee1257391c8d656
Marc Moerkeberg <mc.christiansen@outlook.com>
2025-06-08 16:20:15 +0200

    Add summary for upsertcardcollection

--- CardAtlas.Server/Services/Interfaces/IScryfallIngestionService.cs
+++ CardAtlas.Server/Services/Interfaces/IScryfallIngestionService.cs
@@ -5,10 +5,14 @@ namespace CardAtlas.Server.Services.Interfaces;
 public interface IScryfallIngestionService
 {
 	/// <summary>
-	/// Updates all pre-existing official wotch <see cref="Set"/> entities and adds any new ones.
+	/// Updates all pre-existing official wotc <see cref="Set"/> entities and adds any new ones.
 	/// </summary>
 	/// <returns>The number of affected rows/entities.</returns>
 	Task<int> UpsertSets();
 
+	/// <summary>
+	/// Creates new and updates existing official wotc <see cref="Card"/> entities and their related data.<br/>
+	/// Related entities consists of <see cref="Artist"/>, <see cref="Set"/> etc.
+	/// </summary>
 	Task UpsertCardCollection();
 }
