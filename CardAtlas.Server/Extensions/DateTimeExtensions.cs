721e789a3f36b1e30e73307ac6acb7175beb25bc
Marc Moerkeberg <mc.christiansen@outlook.com>
2025-07-04 20:27:12 +0200

    Add datetime extension for truncating values

---
+++ CardAtlas.Server/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace CardAtlas.Server.Extensions;
+
+public static class DateTimeExtensions
+{
+	/// <summary>
+	/// Truncates the datetime to the specified <paramref name="scale"/>.
+	/// </summary>
+	/// <param name="scale">Determines how far to truncate the <paramref name="target"/>. Use TimeSpan.FromXXX for ease of declaration.</param>
+	/// <returns>A new <see cref="DateTime"/> object truncted to the specified <paramref name="scale"/>.</returns>
+	public static DateTime Truncate(this DateTime target, TimeSpan scale)
+	{
+		if (scale <= TimeSpan.Zero) throw new ArgumentOutOfRangeException(nameof(scale), "Must be greater than zero.");
+
+		return target.AddTicks(-(target.Ticks % scale.Ticks));
+	}
+}
