cca1fb7a37084b5c11d2a2f01e4fe1c5044f590f
Marc Moerkeberg <mc.christiansen@outlook.com>
2025-07-24 15:36:35 +0200

    Add IdentityRoleConfiguration

---
+++ CardAtlas.Server/DAL/EntityConfigurations/RoleConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using CardAtlas.Server.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CardAtlas.Server.DAL.EntityConfigurations;
+
+public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+{
+	public void Configure(EntityTypeBuilder<IdentityRole> builder)
+	{
+		List<IdentityRole> seedData = EntityConfigurationHelper.GetIdentitySeedData();
+
+		builder.HasData(seedData);
+	}
+}
---
+++ CardAtlas.Server/Models/Internal/Roles.cs
@@ -0,0 +1,11 @@
+﻿namespace CardAtlas.Server.Models.Internal;
+
+public static class Roles
+{
+	public const string Admin = "Admin";
+	public const string Moderator = "Moderator";
+	public const string User = "User";
+	public const string Guest = "Guest";
+	public static readonly string[] DefaultRoles = [User];
+	public static readonly string[] AllRoles = [Admin, Moderator, User, Guest];
+}
