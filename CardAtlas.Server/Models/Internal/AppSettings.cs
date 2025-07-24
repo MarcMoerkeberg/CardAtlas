536e70d625e41e1fb369ecf6c4144af5f64097ef
Marc Moerkeberg <mc.christiansen@outlook.com>
2025-07-24 14:50:35 +0200

    Add JwtSettings to AppSettings

--- CardAtlas.Server/Models/Internal/AppSettings.cs
+++ CardAtlas.Server/Models/Internal/AppSettings.cs
@@ -1,12 +1,23 @@
-﻿namespace CardAtlas.Server.Models.Internal;
+﻿using System.Text;
+
+namespace CardAtlas.Server.Models.Internal;
 
 public class AppSettings
 {
 	public required string AppName { get; init; }
 	public required ConnectionStrings ConnectionStrings { get; init; }
+	public required JwtSettings JwtSettings { get; init; }
 }
 
 public record ConnectionStrings
 {
 	public required string Database { get; init; }
+}
+
+public record JwtSettings
+{
+	public required string Secret { get; init; }
+	public byte[] Key => Encoding.UTF8.GetBytes(Secret);
+	public required string Audience { get; init; }
+	public required TimeSpan TimeToLive { get; init; }
 }
