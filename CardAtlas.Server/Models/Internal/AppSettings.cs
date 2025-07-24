9388a7a7084f429a376c05c05dc9395c6047ed1e
Marc Moerkeberg <mc.christiansen@outlook.com>
2025-07-24 14:45:18 +0200

    Move ConnectionStrings class into AppSettings

--- CardAtlas.Server/Models/Internal/AppSettings.cs
+++ CardAtlas.Server/Models/Internal/AppSettings.cs
@@ -3,5 +3,10 @@
 public class AppSettings
 {
 	public required string AppName { get; init; }
-	public required ConnectionStrings ConnectionStrings { get; set; }
+	public required ConnectionStrings ConnectionStrings { get; init; }
+}
+
+public record ConnectionStrings
+{
+	public required string Database { get; init; }
 }
--- CardAtlas.Server/Models/Internal/ConnectionStrings.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CardAtlas.Server.Models.Internal;
-
-public class ConnectionStrings
-{
-	public required string Database { get; init; }
-}
