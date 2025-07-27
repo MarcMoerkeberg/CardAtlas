e81046b03d5af87c6cb4bd89e31a6aa633e2317d
Marc Moerkeberg <mc.christiansen@outlook.com>
2025-07-27 22:14:26 +0200

    Add signInDTO

---
+++ CardAtlas.Server/Models/DTOs/Request/SignInDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CardAtlas.Server.Models.DTOs.Request;
+
+public record SignInDTO
+{
+	[Required]
+	[EmailAddress]
+	public required string Email { get; init; }
+
+	[MinLength(1)]
+	[MaxLength(40)]
+	public required string Password { get; init; }
+}
