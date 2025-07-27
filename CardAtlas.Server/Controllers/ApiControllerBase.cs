5f96474c47730dcc284f4d02149ebb0b1b7500df
Marc Moerkeberg <mc.christiansen@outlook.com>
2025-07-27 22:10:42 +0200

    Add controller base class

---
+++ CardAtlas.Server/Controllers/ApiControllerBase.cs
@@ -0,0 +1,43 @@
+﻿using CardAtlas.Server.Resources.Errors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace CardAtlas.Server.Controllers;
+
+public abstract class ApiControllerBase : ControllerBase
+{
+	protected IActionResult BadRequestProblem(string detail) =>
+		Problem(
+			detail: detail,
+			statusCode: StatusCodes.Status400BadRequest,
+			title: Errors.BadRequest
+		);
+
+	protected IActionResult UnauthorizedProblem(string detail) =>
+		Problem(
+			detail: detail,
+			statusCode: StatusCodes.Status401Unauthorized,
+			title: Errors.Unauthorized
+		);
+
+	protected IActionResult ForbiddenProblem(string detail) =>
+		Problem(
+			detail: detail,
+			statusCode: StatusCodes.Status403Forbidden,
+			title: ReasonPhrases.GetReasonPhrase(StatusCodes.Status403Forbidden)
+		);
+
+	protected IActionResult NotFoundProblem(string detail) =>
+		Problem(
+			detail: detail,
+			statusCode: StatusCodes.Status404NotFound,
+			title: Errors.NotFound
+		);
+
+	protected IActionResult ConflictProblem(string detail) =>
+		Problem(
+			detail: detail,
+			statusCode: StatusCodes.Status409Conflict,
+			title: ReasonPhrases.GetReasonPhrase(StatusCodes.Status409Conflict)
+		);
+}
