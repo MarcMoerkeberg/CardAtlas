fa0cb2730054c029d2bfcb870578d2bcf55b2622
Marc Moerkeberg <mc.christiansen@outlook.com>
2025-07-27 22:14:17 +0200

    Implement credential verification and token creation methods in authentication service

--- CardAtlas.Server/Services/AuthenticationService.cs
+++ CardAtlas.Server/Services/AuthenticationService.cs
@@ -1,32 +1,64 @@
-﻿using CardAtlas.Server.Mappers;
+﻿using CardAtlas.Server.Exceptions;
+using CardAtlas.Server.Helpers;
 using CardAtlas.Server.Models.Data;
 using CardAtlas.Server.Models.DTOs.Request;
 using CardAtlas.Server.Models.Internal;
 using CardAtlas.Server.Repositories.Interfaces;
+using CardAtlas.Server.Resources.Errors;
 using CardAtlas.Server.Services.Interfaces;
 using Microsoft.AspNetCore.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Security.Claims;
 
 namespace CardAtlas.Server.Services;
 
 public class AuthenticationService : IAuthenticationService
 {
+	private readonly AppSettings _appSettings;
 	private readonly IUserRepository _userRepository;
+	private readonly UserManager<User> _userManager;
 
-	public AuthenticationService(IUserRepository userRepository)
+	public AuthenticationService(
+		AppSettings appSettings,
+		IUserRepository userRepository,
+		UserManager<User> userManager)
 	{
+		_appSettings = appSettings;
 		_userRepository = userRepository;
+		_userManager = userManager;
 	}
 
-	public async Task<IdentityResult> CreateUserAsync(SignUpDTO signUpDTO)
+	public async Task<bool> VerifyUserCredentials(SignInDTO authenticateDTO)
 	{
-		User newUser = UserMapper.FromDTO(signUpDTO);
+		User? user = await _userManager.FindByEmailAsync(authenticateDTO.Email);
 
-		IdentityResult createUserResult = await _userRepository.CreateAsync(
-			userToCreate: newUser,
-			password: signUpDTO.Password,
-			roles: Roles.DefaultRoles
-		);
+		return user is not null && await _userManager.CheckPasswordAsync(user, authenticateDTO.Password);
+	}
 
-		return createUserResult;
+	public async Task<string> CreateToken(string email)
+	{
+		List<Claim>? claims = await _userRepository.GetClaimsAsync(email);
+		if (claims is null)
+		{
+			throw new HttpException(
+				statusCode: HttpStatusCode.NotFound,
+				message: string.Format(Errors.UserNotFoundWithEmail, email),
+				title: Errors.UserNotFound
+			);
+		}
+		else if (!claims.Any(claim => claim.Type == ClaimTypes.Role))
+		{
+			throw new HttpException(
+				statusCode: HttpStatusCode.Forbidden,
+				message: string.Format(Errors.UserHasNoRolesWithEmail, email),
+				title: Errors.NoRolesAssigned
+			);
+		}
+
+		JwtSecurityTokenHandler tokenHandler = new();
+		JwtSecurityToken token = AuthenticationHelper.GenerateSecurityToken(claims, _appSettings);
+
+		return tokenHandler.WriteToken(token);
 	}
 }
--- CardAtlas.Server/Services/Interfaces/IAuthenticationService.cs
+++ CardAtlas.Server/Services/Interfaces/IAuthenticationService.cs
@@ -1,9 +1,20 @@
-﻿using CardAtlas.Server.Models.DTOs.Request;
-using Microsoft.AspNetCore.Identity;
+﻿using CardAtlas.Server.Models.Data;
+using CardAtlas.Server.Models.DTOs.Request;
 
 namespace CardAtlas.Server.Services.Interfaces;
 
 public interface IAuthenticationService
 {
-	Task<IdentityResult> CreateUserAsync(SignUpDTO signUpDTO);
+	/// <summary>
+	/// Verifies the credentials of the user.
+	/// </summary>
+	/// <returns>Returns true if a user with the specified credentials exist; otherwise false.</returns>
+	Task<bool> VerifyUserCredentials(SignInDTO authenticateDTO);
+
+	/// <summary>
+	/// Creates a jwt from the <see cref="User"/> associated with the <paramref name="email"/>.
+	/// </summary>
+	/// <returns>A serialized jwt for the <see cref="User"/> associated with the <paramref name="email"/>.</returns>
+	Task<string> CreateToken(string email);
+
 }
