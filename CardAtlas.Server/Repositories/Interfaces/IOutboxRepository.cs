c399145bf166ef314cc9b4180f5083de11394fe6
Marc Moerkeberg <mc.christiansen@outlook.com>
2025-08-07 21:24:43 +0200

    Add outbox repository

---
+++ CardAtlas.Server/Repositories/Interfaces/IOutboxRepository.cs
@@ -0,0 +1,58 @@
+﻿using CardAtlas.Server.Models.Entities;
+
+namespace CardAtlas.Server.Repositories.Interfaces
+{
+	public interface IOutboxRepository
+	{
+		/// <summary>
+		/// Returns all <see cref="OutboxMessage"/> entities.
+		/// </summary>
+		/// <returns>All <see cref="OutboxMessage"/> entities in the db.</returns>
+		Task<List<OutboxMessage>> GetAsync();
+
+		/// <summary>
+		/// Returns all <see cref="OutboxMessage"/> entities matching <paramref name="isProcessed"/> and <paramref name="maxRetries"/>.
+		/// </summary>
+		/// <param name="maxRetries">Skips entities with <see cref="OutboxMessage.RetryCount"/> that exceeds <paramref name="maxRetries"/>.</param>
+		/// <returns>All <see cref="OutboxMessage"/> entities matching the specified pattern in the db.</returns>
+		Task<List<OutboxMessage>> GetAsync(bool isProcessed, int maxRetries = 5);
+
+		/// <summary>
+		/// Finds <see cref="OutboxMessage"/> entities matching <paramref name="isProcessed"/> and <paramref name="maxRetries"/>.
+		/// </summary>
+		/// <param name="maxRetries">Skips entities with <see cref="OutboxMessage.RetryCount"/> that exceeds <paramref name="maxRetries"/>.</param>
+		/// <param name="batchSize">Determines the maximum amount of entities returned in the list.</param>
+		/// <returns>A list <see cref="OutboxMessage"/> entities matching the specified pattern in the db. List will contain at most x entries based on the <paramref name="batchSize"/>.</returns>
+		Task<List<OutboxMessage>> GetAsync(bool isProcessed, int batchSize, int maxRetries = 5);
+
+		/// <summary>
+		/// Finds the <see cref="OutboxMessage"/> associated with the <paramref name="id"/>.
+		/// </summary>
+		/// <returns>The <see cref="OutboxMessage"/> associated with the <paramref name="id"/>.</returns>
+		Task<OutboxMessage> GetAsync(Guid id);
+
+		/// <summary>
+		/// Finds all <see cref="OutboxMessage"/> entities associated with the <paramref name="ids"/>.
+		/// </summary>
+		/// <returns>All <see cref="OutboxMessage"/> entities associated with the <paramref name="ids"/>.</returns>
+		Task<List<OutboxMessage>> GetAsync(IEnumerable<Guid> ids);
+
+		/// <summary>
+		/// Adds the <paramref name="outboxMessage"/> to the database.
+		/// </summary>
+		/// <returns>The added <see cref="OutboxMessage"/> with identity.</returns>
+		Task<OutboxMessage> CreateAsync(OutboxMessage outboxMessage);
+
+		/// <summary>
+		/// Updates the <see cref="OutboxMessage"/> entity correlating with <paramref name="outboxMessage"/>.
+		/// </summary>
+		/// <returns>The updated <see cref="OutboxMessage"/>.</returns>
+		Task<OutboxMessage> UpdateAsync(OutboxMessage outboxMessage);
+
+		/// <summary>
+		/// Updates the <see cref="OutboxMessage"/> entities correlating with the ones in <paramref name="outboxMessages"/>.
+		/// </summary>
+		/// <returns>The number of updated <see cref="OutboxMessage"/> entities.</returns>
+		Task<int> UpdateAsync(IEnumerable<OutboxMessage> outboxMessages);
+	}
+}
---
+++ CardAtlas.Server/Repositories/OutboxRepository.cs
@@ -0,0 +1,102 @@
+﻿using CardAtlas.Server.DAL;
+using CardAtlas.Server.Models.Entities;
+using CardAtlas.Server.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace CardAtlas.Server.Repositories;
+
+public class OutboxRepository : IOutboxRepository
+{
+	private readonly IDbContextFactory<ApplicationDbContext> _dbContextFactory;
+
+	public OutboxRepository(IDbContextFactory<ApplicationDbContext> dbContextFactory)
+	{
+		_dbContextFactory = dbContextFactory;
+	}
+
+	public async Task<List<OutboxMessage>> GetAsync()
+	{
+		using ApplicationDbContext dbContext = _dbContextFactory.CreateDbContext();
+
+		return await dbContext.OutboxMessages
+			.AsNoTracking()
+			.ToListAsync();
+	}
+
+	public async Task<List<OutboxMessage>> GetAsync(bool isProcessed, int maxRetries = 5)
+	{
+		using ApplicationDbContext dbContext = _dbContextFactory.CreateDbContext();
+
+		return await dbContext.OutboxMessages
+			.AsNoTracking()
+			.Where(message =>
+				message.IsProcessed == isProcessed &&
+				message.RetryCount <= maxRetries
+			)
+			.OrderBy(message => message.CreatedDate)
+			.ToListAsync();
+	}
+
+	public async Task<List<OutboxMessage>> GetAsync(bool isProcessed, int batchSize, int maxRetries = 5)
+	{
+		using ApplicationDbContext dbContext = _dbContextFactory.CreateDbContext();
+
+		return await dbContext.OutboxMessages
+			.AsNoTracking()
+			.Where(message =>
+				message.IsProcessed == isProcessed &&
+				message.RetryCount <= maxRetries
+			)
+			.OrderBy(message => message.CreatedDate)
+			.Take(batchSize)
+			.ToListAsync();
+	}
+
+	public async Task<OutboxMessage> GetAsync(Guid id)
+	{
+		using ApplicationDbContext dbContext = _dbContextFactory.CreateDbContext();
+
+		return await dbContext.OutboxMessages
+			.AsNoTracking()
+			.SingleAsync(message => message.Id == id);
+	}
+
+	public async Task<List<OutboxMessage>> GetAsync(IEnumerable<Guid> ids)
+	{
+		using ApplicationDbContext dbContext = _dbContextFactory.CreateDbContext();
+
+		return await dbContext.OutboxMessages
+			.AsNoTracking()
+			.Where(message => ids.Contains(message.Id))
+			.ToListAsync();
+	}
+
+	public async Task<OutboxMessage> CreateAsync(OutboxMessage outboxMessage)
+	{
+		using ApplicationDbContext dbContext = _dbContextFactory.CreateDbContext();
+
+		EntityEntry<OutboxMessage> addedMessage = dbContext.OutboxMessages.Add(outboxMessage);
+		await dbContext.SaveChangesAsync();
+
+		return addedMessage.Entity;
+	}
+
+	public async Task<OutboxMessage> UpdateAsync(OutboxMessage outboxMessage)
+	{
+		using ApplicationDbContext dbContext = _dbContextFactory.CreateDbContext();
+
+		EntityEntry<OutboxMessage> updatedMessage = dbContext.OutboxMessages.Update(outboxMessage);
+		await dbContext.SaveChangesAsync();
+
+		return updatedMessage.Entity;
+	}
+
+	public async Task<int> UpdateAsync(IEnumerable<OutboxMessage> outboxMessages)
+	{
+		using ApplicationDbContext dbContext = _dbContextFactory.CreateDbContext();
+		dbContext.OutboxMessages.UpdateRange(outboxMessages);
+
+		return await dbContext.SaveChangesAsync();
+	}
+}
